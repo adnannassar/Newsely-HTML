a3360a0c563b594af69c76950d1fc7de8e821ccb
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-06-29 17:32:18 +0200

    added ome fixes and improvements to code

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.use(router);
 app.set('view engine', 'ejs');
 app.set("views","views");
 
-
 const port = process.env.port || 3000;
 app.listen(port, function () {
     console.log("Server is running on:", port)
--- routes/routes.js
+++ routes/routes.js
@@ -8,13 +8,14 @@ const API_KEY_2_NEWS="78c2fbbc39c140afa466d92ed5f79b85"
 const API_KEY_3_NEWS="2531ba8806204059af4a5e4fe053efde"
 const API_KEY_WEATHER="41ad88a1f768b910e390f99a84773acb"
 const API_KEY_MAPS="AIzaSyC9Mk7AvKHkzPSOffPuLg_HlhzP7wAPGEM"
-const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY_1_NEWS}&pageSize=1`;
-const topHeadLinesInGermany = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${API_KEY_1_NEWS}&pageSize=3`;
-const businessInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
-const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
-const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${API_KEY_1_NEWS}&pageSize=7`;
+
 
 router.get('/', async (req, res) => {
+    const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY_1_NEWS}&pageSize=1`;
+    const topHeadLinesInGermany = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${API_KEY_1_NEWS}&pageSize=3`;
+    const businessInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
+    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
+    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${API_KEY_1_NEWS}&pageSize=7`;
     try {
         const internationalHeadlines = await axios.get(topInternationalHeadLines);
         const germanyHeadlines = await axios.get(topHeadLinesInGermany);
@@ -42,12 +43,33 @@ router.get('/', async (req, res) => {
     }
 });
 
-const entertainmentInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=entertainment&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
 router.get("/unterhaltung", async (req, res) => {
+    const entertainmentInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=entertainment&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
     try {
         const entertainmentNewsGermany = await axios.get(entertainmentInGermany);
         res.render('unterhaltung', {
             entertainmentNewsGermanyArtikels: entertainmentNewsGermany.data.articles
+        });
+    } catch (error) {
+
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('ERROR', error.message);
+        }
+    }
+});
+
+router.get("/gesundheit", async function (req, res) {
+    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    try {
+        const healthNewsGermany = await axios.get(healthInGermany);
+        res.render('gesundheit', {
+            healthNewsGermanyArtikels: healthNewsGermany.data.articles
         });
     } catch (error) {
         if (error.response) {
@@ -59,29 +81,99 @@ router.get("/unterhaltung", async (req, res) => {
         } else {
             console.log('ERROR', error.message);
         }
-
     }
+
 });
 
-router.get("/gesundheit", function (req, res) {
-    res.render("gesundheit");
+router.get("/allgemeines", async function (req, res) {
+    const generalInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=general&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    try {
+        const generalNewsGermany = await axios.get(generalInGermany);
+        res.render('allgemeines', {
+            generalNewsGermanyArtikels: generalNewsGermany.data.articles
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('ERROR', error.message);
+        }
+    }
+
 });
 
-router.get("/allgemeines", function (req, res) {
-    res.render("allgemeines");
+router.get("/wissenschaft", async function (req, res) {
+    const scienceInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    try {
+        const scienceNewsGermany = await axios.get(scienceInGermany);
+        res.render('wissenschaft', {
+            scienceNewsGermanyArtikels: scienceNewsGermany.data.articles
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('ERROR', error.message);
+        }
+    }
+
 });
 
-router.get("/wissenschaft", function (req, res) {
-    res.render("wissenschaft");
+router.get("/sport", async function (req, res) {
+    const sportsInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    try {
+        const sportsNewsGermany = await axios.get(sportsInGermany);
+        res.render('sport', {
+            sportsNewsGermanyArtikels: sportsNewsGermany.data.articles
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('ERROR', error.message);
+        }
+    }
+
 });
 
-router.get("/sport", function (req, res) {
-    res.render("sport");
+router.get("/technologie", async function (req, res) {
+    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    try {
+        const technologyNewsGermany = await axios.get(technologyInGermany);
+        res.render('technologie', {
+            technologyGermanyArtikels: technologyNewsGermany.data.articles
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('ERROR', error.message);
+        }
+    }
+
 });
 
-router.get("/technologie", function (req, res) {
-    res.render("technologie");
+router.get("/about", function (req, res) {
+    res.render("about");
 });
-
+router.use(function (req, res) {
+    res.render('404');
+});
 
 module.exports = router;
