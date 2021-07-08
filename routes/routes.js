d1f3e46f81a9e2a0426cbc250af13917b3dfbeb9
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2021-07-08 12:27:35 +0200

    fixed changed API-Key

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,37 @@
-
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const API_KEY_1_NEWS = '78c2fbbc39c140afa466d92ed5f79b85';
-const API_KEY_2_NEWS="78c2fbbc39c140afa466d92ed5f79b85"
-const API_KEY_3_NEWS="2531ba8806204059af4a5e4fe053efde"
-const API_KEY_WEATHER="41ad88a1f768b910e390f99a84773acb"
-const API_KEY_MAPS="AIzaSyC9Mk7AvKHkzPSOffPuLg_HlhzP7wAPGEM"
+function getAPIKey(keyNumber) {
+    const API_KEY_1_NEWS = '78c2fbbc39c140afa466d92ed5f79b85';
+    const API_KEY_2_NEWS = "78c2fbbc39c140afa466d92ed5f79b85"
+    const API_KEY_3_NEWS = "2531ba8806204059af4a5e4fe053efde"
+    const API_KEY_4_NEWS = "5952cbd45a9a4d14bac6d5a01c388f12"
 
+    switch (keyNumber) {
+        case 1:
+            return API_KEY_1_NEWS;
+            break;
+        case 2:
+            return API_KEY_2_NEWS;
+            break;
+        case 3:
+            return API_KEY_3_NEWS;
+            break;
+        case 4:
+            return API_KEY_4_NEWS;
+            break;
+    }
+}
+
+const apiKeyNumber = 3;
 
 router.get('/', async (req, res) => {
-    const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY_1_NEWS}&pageSize=1`;
-    const topHeadLinesInGermany = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${API_KEY_1_NEWS}&pageSize=3`;
-    const businessInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
-    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${API_KEY_1_NEWS}&pageSize=6`;
-    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${API_KEY_1_NEWS}&pageSize=7`;
+    const topInternationalHeadLines = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=1`;
+    const topHeadLinesInGermany = `https://newsapi.org/v2/top-headlines?country=de&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=3`;
+    const businessInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=6`;
+    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=6`;
+    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=7`;
     try {
         const internationalHeadlines = await axios.get(topInternationalHeadLines);
         const germanyHeadlines = await axios.get(topHeadLinesInGermany);
@@ -44,7 +60,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get("/unterhaltung", async (req, res) => {
-    const entertainmentInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=entertainment&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const entertainmentInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=entertainment&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const entertainmentNewsGermany = await axios.get(entertainmentInGermany);
         res.render('unterhaltung', {
@@ -65,7 +81,8 @@ router.get("/unterhaltung", async (req, res) => {
 });
 
 router.get("/gesundheit", async function (req, res) {
-    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=health&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const healthInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=
+    health&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const healthNewsGermany = await axios.get(healthInGermany);
         res.render('gesundheit', {
@@ -86,7 +103,7 @@ router.get("/gesundheit", async function (req, res) {
 });
 
 router.get("/allgemeines", async function (req, res) {
-    const generalInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=general&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const generalInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=general&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const generalNewsGermany = await axios.get(generalInGermany);
         res.render('allgemeines', {
@@ -107,7 +124,7 @@ router.get("/allgemeines", async function (req, res) {
 });
 
 router.get("/wissenschaft", async function (req, res) {
-    const scienceInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const scienceInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=science&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const scienceNewsGermany = await axios.get(scienceInGermany);
         res.render('wissenschaft', {
@@ -128,7 +145,7 @@ router.get("/wissenschaft", async function (req, res) {
 });
 
 router.get("/sport", async function (req, res) {
-    const sportsInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const sportsInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=sports&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const sportsNewsGermany = await axios.get(sportsInGermany);
         res.render('sport', {
@@ -149,7 +166,7 @@ router.get("/sport", async function (req, res) {
 });
 
 router.get("/technologie", async function (req, res) {
-    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${API_KEY_1_NEWS}&pageSize=12`;
+    const technologyInGermany = `https://newsapi.org/v2/top-headlines?country=de&category=technology&apiKey=${getAPIKey(apiKeyNumber)}&pageSize=12`;
     try {
         const technologyNewsGermany = await axios.get(technologyInGermany);
         res.render('technologie', {
